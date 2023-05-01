b77d4be8c75a022bf0d7ac9fbd30d32e6ec9f8a2
Paulina Radomska <113101087+pradomska@users.noreply.github.com>
2023-05-01 14:12:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import requests
 from datetime import datetime
 import os
 
-# Your personal data. Used by Nutritionix to calculate calories.
+# My personal data. Used by Nutritionix to calculate calories.
 GENDER = "male"
-WEIGHT_KG = 84
-HEIGHT_CM = 180
-AGE = 32
+WEIGHT_KG = 69
+HEIGHT_CM = 169
+AGE = 24
 
 # Nutritionix APP ID and API Key. Actual values are stored as environment variables.
 APP_ID = os.environ["ENV_NIX_APP_ID"]
 API_KEY = os.environ["ENV_NIX_API_KEY"]
+SHEETY_TOKEN = os.environ["ENV_SHEETY_TOKEN"]
+GOOGLE_SHEET_NAME = 'workout'
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-
 
 exercise_text = input("Tell me which exercises you did: ")
 
@@ -39,10 +40,7 @@ print(f"Nutritionix API call: \n {result} \n")
 today_date = datetime.now().strftime("%d/%m/%Y")
 now_time = datetime.now().strftime("%X")
 
-# Sheety Project API. Check your Google sheet name and Sheety endpoint
-GOOGLE_SHEET_NAME = "workout"
-sheet_endpoint = os.environ[
-    "ENV_SHEETY_ENDPOINT"]
+sheet_endpoint = os.environ["ENV_SHEETY_ENDPOINT"]
 
 # Sheety API Call & Authentication
 for exercise in result["exercises"]:
@@ -56,30 +54,13 @@ for exercise in result["exercises"]:
         }
     }
 
-    # Sheety Authentication Option 1: No Auth
-    """
-    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
-    """
-
-    # Sheety Authentication Option 2: Basic Auth
-    sheet_response = requests.post(
-        sheet_endpoint,
-        json=sheet_inputs,
-        auth=(
-            os.environ["ENV_SHEETY_USERNAME"],
-            os.environ["ENV_SHEETY_PASSWORD"],
-        )
-    )
-
-    # Sheety Authentication Option 3: Bearer Token
-    """
     bearer_headers = {
-        "Authorization": f"Bearer {os.environ['ENV_SHEETY_TOKEN']}"
+        "Authorization": f"Bearer {SHEETY_TOKEN}"
     }
     sheet_response = requests.post(
         sheet_endpoint,
         json=sheet_inputs,
         headers=bearer_headers
-    )    
-    """
+    )
+
     print(f"Sheety Response: \n {sheet_response.text}")
